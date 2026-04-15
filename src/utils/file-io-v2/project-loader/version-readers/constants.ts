f877cce8063761220655e8c3b69e936d4610aced
Andrea Papaleo <apapaleo3@icloud.com>
2026-04-15 16:23:55 -0400

    feat(project-reader): add readers for old projects (0.1.x, 0.2.x, 1.1.x)

---
+++ src/utils/file-io-v2/project-loader/version-readers/constants.ts
@@ -0,0 +1,47 @@
+import {
+  CropSchema,
+  LossFunction,
+  Metric,
+  OptimizationAlgorithm,
+} from "utils/models/enums";
+import { V01ClassifierState } from "./version-types/v01Types";
+
+export const initialClassifierStateV01_02: V01ClassifierState = {
+  modelStatus: 0,
+  inputShape: {
+    planes: 1,
+    height: 64,
+    width: 64,
+    channels: 3,
+  },
+  fitOptions: {
+    epochs: 10,
+    batchSize: 32,
+  },
+  learningRate: 0.01,
+  lossFunction: LossFunction.CategoricalCrossEntropy,
+  selectedModelIdx: 0,
+  metrics: [Metric.CategoricalAccuracy],
+  optimizationAlgorithm: OptimizationAlgorithm.Adam,
+  preprocessOptions: {
+    shuffle: true,
+    rescaleOptions: {
+      rescale: true,
+      center: false,
+    },
+    cropOptions: {
+      numCrops: 1,
+      cropSchema: CropSchema.None,
+    },
+  },
+  trainingPercentage: 0.75,
+  evaluationResult: {
+    confusionMatrix: [],
+    accuracy: -1,
+    crossEntropy: -1,
+    precision: -1,
+    recall: -1,
+    f1Score: -1,
+  },
+  showClearPredictionsWarning: true,
+};
