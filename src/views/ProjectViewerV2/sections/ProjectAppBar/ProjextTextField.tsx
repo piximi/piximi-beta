ebbd87e695e9c3ad6506cb0c9df81c7ac62d0f95
Andrea Papaleo <apapaleo3@icloud.com>
2026-04-18 17:58:56 -0400

    refactor(project-viewer): remove conditional load message display from `ProjectTextField`

--- src/views/ProjectViewerV2/sections/ProjectAppBar/ProjextTextField.tsx
+++ src/views/ProjectViewerV2/sections/ProjectAppBar/ProjextTextField.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { FormControl, Typography } from "@mui/material";
+import { FormControl } from "@mui/material";
 
 import { TextFieldWithBlur } from "components/inputs";
 
-import { selectLoadMessage } from "store/applicationSettings/selectors";
 import { projectSlice } from "store/project";
 import { selectProjectName } from "store/project/selectors";
 import { HelpItem } from "components/layout/HelpDrawer/HelpContent";
@@ -12,7 +11,6 @@ import { HelpItem } from "components/layout/HelpDrawer/HelpContent";
 export const ProjectTextField = () => {
   const dispatch = useDispatch();
 
-  const loadMessage = useSelector(selectLoadMessage);
   const projectName = useSelector(selectProjectName);
   const [newProjectName, setNewProjectName] = useState<string>(projectName);
   const inputRef = useRef<HTMLInputElement>(null);
@@ -34,33 +32,25 @@ export const ProjectTextField = () => {
   }, [projectName]);
 
   return (
-    <>
-      {loadMessage ? (
-        <Typography ml={5} sx={{ flexGrow: 1 }}>
-          {loadMessage}
-        </Typography>
-      ) : (
-        <FormControl>
-          <TextFieldWithBlur
-            data-help={HelpItem.ProjectName}
-            onChange={handleTextFieldChange}
-            onBlur={handleTextFieldBlur}
-            value={newProjectName}
-            inputRef={inputRef}
-            size="small"
-            sx={{ ml: 5 }}
-            variant="standard"
-            slotProps={{
-              htmlInput: { min: 0 },
-              input: {
-                slotProps: {
-                  input: { min: 0 },
-                },
-              },
-            }}
-          />
-        </FormControl>
-      )}
-    </>
+    <FormControl>
+      <TextFieldWithBlur
+        data-help={HelpItem.ProjectName}
+        onChange={handleTextFieldChange}
+        onBlur={handleTextFieldBlur}
+        value={newProjectName}
+        inputRef={inputRef}
+        size="small"
+        sx={{ ml: 5 }}
+        variant="standard"
+        slotProps={{
+          htmlInput: { min: 0 },
+          input: {
+            slotProps: {
+              input: { min: 0 },
+            },
+          },
+        }}
+      />
+    </FormControl>
   );
 };
