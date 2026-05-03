d22d733afce1e6f8780853833f91984ff46e899f
Andrea Papaleo <apapaleo3@icloud.com>
2026-05-02 22:48:49 -0400

    chore(project-state): prune unused listeners

--- src/views/ProjectViewer/state/projectListeners.ts
+++ src/views/ProjectViewer/state/projectListeners.ts
@@ -1,8 +1,6 @@
 import { createListenerMiddleware } from "@reduxjs/toolkit";
-import { difference, intersection } from "lodash";
 
 import { classifierSlice } from "store/classifier";
-import { dataSlice } from "store/data";
 import type { TypedAppStartListening } from "store/types";
 import { segmenterSlice } from "store/segmenter";
 import { imageViewerSlice } from "views/ImageViewer/state/imageViewer";
@@ -20,7 +18,6 @@ const startAppListening =
 startAppListening({
   actionCreator: projectSlice.actions.resetProject,
   effect: (action, listenerAPI) => {
-    listenerAPI.dispatch(dataSlice.actions.resetData());
     listenerAPI.dispatch(dataSliceV2.actions.clearState());
     listenerAPI.dispatch(classifierSlice.actions.resetClassifiers());
     listenerAPI.dispatch(segmenterSlice.actions.resetSegmenter());
@@ -39,61 +36,6 @@ startAppListening({
 startAppListening({
   predicate: (action, currentState, previousState) => {
     return (
-      currentState.data.categories.ids.length <
-      previousState.data.categories.ids.length
-    );
-  },
-  effect: async (action, listenerApi) => {
-    const { project, data } = listenerApi.getState();
-    const { data: oldData } = listenerApi.getOriginalState();
-    const deletedCategories = difference(
-      oldData.categories.ids,
-      data.categories.ids,
-    ) as string[];
-    const filters = project.thingFilters;
-    for (const kind in filters) {
-      const filteredCats = filters[kind].categoryId;
-      const deletedFilters = intersection(filteredCats, deletedCategories);
-      if (deletedFilters.length > 0) {
-        listenerApi.dispatch(
-          projectSlice.actions.removeThingCategoryFilters({
-            categoryIds: deletedFilters,
-            kinds: [kind],
-          }),
-        );
-      }
-    }
-  },
-});
-
-startAppListening({
-  actionCreator: dataSlice.actions.deleteKind,
-  effect: async (action, listenerApi) => {
-    const { data } = listenerApi.getState();
-    const { data: oldData } = listenerApi.getOriginalState();
-    const deletedKinds = difference(
-      oldData.kinds.ids,
-      data.kinds.ids,
-    ) as string[];
-
-    listenerApi.dispatch(
-      projectSlice.actions.removeThingCategoryFilters({
-        categoryIds: "all",
-        kinds: deletedKinds,
-      }),
-    );
-    listenerApi.dispatch(
-      projectSlice.actions.removeThingPartitionFilters({
-        partitions: "all",
-        kinds: deletedKinds,
-      }),
-    );
-  },
-});
-
-startAppListening({
-  predicate: (action, currentState, previousState) => {
-    return (
       currentState.project.imageChannels !== previousState.project.imageChannels
     );
   },
@@ -106,35 +48,5 @@ startAppListening({
           globalChannels: project.imageChannels,
         }),
       );
-  },
-});
-
-startAppListening({
-  actionCreator: dataSlice.actions.deleteThings,
-  effect: (action, listenerAPI) => {
-    const { project, data } = listenerAPI.getState();
-    if (action.payload.preparedByListener && "thingIds" in action.payload) {
-      const { thingIds } = action.payload;
-      const selectedThings = project.selectedThingIds;
-      const implicitThingIds: string[] = [];
-      for (const thingId of thingIds) {
-        const thing = data.things.entities[thingId];
-        if (!thing) continue;
-        if ("containing" in thing) {
-          const containedThingIds = thing.containing;
-          implicitThingIds.push(...containedThingIds);
-        }
-      }
-      const deletedThingsToDeselect = intersection(
-        [...thingIds, ...implicitThingIds],
-        selectedThings,
-      );
-
-      listenerAPI.dispatch(
-        projectSlice.actions.deselectThings({ ids: deletedThingsToDeselect }),
-      );
-    }
-
-    return;
   },
 });
