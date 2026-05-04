d0b48bb83a0f2cae2a14ad7521e883dd9a05f0b1
Andrea <93597832+Andrea-Papaleo@users.noreply.github.com>
2026-05-03 20:59:01 -0400

    refactor(state): update `useParameterizedSelector` to accept arbitrary number of arguments

--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -1,9 +1,9 @@
 import { useSelector } from "react-redux";
 import { RootState } from "./rootReducer";
 
-export function useParameterizedSelector<P, R>(
-  selector: (state: RootState, param: P) => R,
-  param: P,
+export function useParameterizedSelector<P extends unknown[], R>(
+  selector: (state: RootState, ...params: P) => R,
+  ...params: P
 ): R {
-  return useSelector((state: RootState) => selector(state, param));
+  return useSelector((state: RootState) => selector(state, ...params));
 }
