4abb9960218240091ff0c2a54f5291118a046dde
Andrea Papaleo <apapaleo3@icloud.com>
2026-05-15 12:35:24 -0400

    feat(classifier)[types]: create types for interacting with classifier worker

---
+++ src/utils/dl/classification/worker/dto.ts
@@ -0,0 +1,42 @@
+import type { ModelArch, RunHistoryEpoch } from "store/classifier/types";
+
+import type { ModelTask } from "utils/dl/enums";
+import type {
+  ModelLayerData,
+  OptimizerSettings,
+  PreprocessSettings,
+} from "utils/dl/types";
+
+export type ModelInfoDTO = {
+  name: string;
+  archTag: ModelArch | string | undefined;
+  task: ModelTask;
+  graph: boolean;
+  trainable: boolean;
+  pretrained: boolean;
+  classes: string[];
+  numClasses: number;
+  preprocessingOptions: PreprocessSettings | undefined;
+  optimizerSettings: OptimizerSettings | undefined;
+  currentFitHistory: RunHistoryEpoch[];
+  modelSummary: ModelLayerData[] | undefined;
+  modelLoaded: boolean;
+  trainingLoaded: boolean;
+  validationLoaded: boolean;
+  inferenceLoaded: boolean;
+  defaultInputShape: number[] | undefined;
+  defaultOutputShape: number[] | undefined;
+};
+
+export type ModelLoadResult =
+  | { success: true; modelInfo: ModelInfoDTO }
+  | {
+      success: false;
+      modelName: string;
+      error: { reason: string; err?: Error };
+    };
+
+export type BatchModelLoadResult = {
+  loadedModelInfos: ModelInfoDTO[];
+  failedModels: Record<string, { reason: string; err?: Error }>;
+};
