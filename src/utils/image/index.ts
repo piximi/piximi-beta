c508ceb76da3db6a40b1c1b8c92fc158b1738ea3
Andrea Papaleo <apapaleo3@icloud.com>
2026-06-04 12:54:12 -0400

    chore(utils): move generic utils from `ImageViewer/` directory to global `utils/`

---
+++ src/utils/image/index.ts
@@ -0,0 +1,15 @@
+export { scanline } from "./scanline";
+
+export { simplifyPolygon } from "./simplify";
+
+export { decodeRleArray, rleEncodeArray } from "./rle";
+
+export {
+  getIdx,
+  connectPoints,
+  drawRectangle,
+  getAnnotationsInBox,
+  getOverlappingAnnotations,
+  colorOverlayROI,
+  hexToRGBA,
+} from "./imageHelper";
---
+++ src/utils/image/rle.ts
@@ -0,0 +1,77 @@
+/**
+ * Decode a Run-length encoded input array.
+ * @param encoded Run-length encoded input array
+ * @returns The decoded input array
+ */
+export const decodeRleArray = (encoded: Array<number>): Uint8ClampedArray => {
+  const decoded = [];
+
+  let background = true;
+
+  for (let i = 0; i < encoded.length; i++) {
+    for (let j = 0; j < encoded[i]; j++) {
+      decoded.push(background ? 0 : 255);
+    }
+
+    background = !background;
+  }
+
+  return new Uint8ClampedArray(decoded);
+};
+
+/**
+ * Compute the Run-length encoding of the input array.
+ * @param decoded (decoded) input array
+ * @param expectBinary true if decoded mask is binary array (only consists of the values 0 or 1)
+                       false if the two values are 0 and 26^bitDepth-1
+ * @returns Encoded array
+ */
+export const rleEncodeArray = (
+  decoded: Uint8Array | Uint8ClampedArray | Uint16Array | Float32Array,
+  expectBinary: boolean = false,
+): Array<number> => {
+  let highVal: number;
+
+  if (expectBinary) {
+    highVal = 1;
+  } else {
+    const bitDepth =
+      decoded.constructor === Uint16Array
+        ? 16
+        : decoded.constructor === Float32Array
+          ? 32
+          : 8; // Uint8[Clamped]Array
+
+    highVal = 2 ** bitDepth - 1;
+  }
+
+  let lastElement = decoded[0];
+
+  let lastSequenceSize = 1;
+
+  const encoded = [];
+
+  // Float32Array data usually holds normalized data between 0 and 1,
+  // in which case it must be denormalized before calling this func
+  // such that 0 -> 0 and 1 -> 2*16-1
+  // or expectBinary should be set true
+  if (decoded[0] === highVal) {
+    encoded.push(0);
+  }
+
+  for (let i = 1; i < decoded.length; i++) {
+    if (lastElement !== decoded[i]) {
+      encoded.push(lastSequenceSize);
+
+      lastElement = decoded[i];
+
+      lastSequenceSize = 1;
+    } else {
+      lastSequenceSize += 1;
+    }
+  }
+
+  encoded.push(lastSequenceSize);
+
+  return encoded;
+};
