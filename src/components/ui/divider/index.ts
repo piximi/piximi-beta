a80c60db6107a10f7f8bbf2f29bb03089372f355
Andrea Papaleo <apapaleo3@icloud.com>
2026-07-17 22:43:22 -0400

    feat(ui): add `PartialDivider` component

--- src/components/ui/divider/index.ts
+++ src/components/ui/divider/index.ts
@@ -1,3 +1,4 @@
 export { DividerHeader } from "./DividerHeader";
 export { DividerWithLoading } from "./DividerWithLoading";
 export { FunctionalDivider } from "./FunctionalDivider";
+export { PartialDivider } from "./PartialDivider";
